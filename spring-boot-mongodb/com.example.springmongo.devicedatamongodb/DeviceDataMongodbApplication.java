5c5fe16777357b4880584484b62cc19d7e5e6bca
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-12 01:21:47 +0530

    Create DeviceDataMongodbApplication.java

---
+++ spring-boot-mongodb/com.example.springmongo.devicedatamongodb/DeviceDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmongo.devicedatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceDataMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceDataMongodbApplication.class, args);
+    }
+
+}
