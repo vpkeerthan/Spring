73d415d54d21ceb8be659b74d8ef1cf7ccc70dee
vpkeerthan <53893152+vpkeerthan@users.noreply.github.com>
2019-09-11 15:58:10 +0530

    Create CrudrepoApplication.java

---
+++ spring-boot-JPA/CrudrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjparepo.crudrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudrepoApplication.class, args);
+    }
+
+}
